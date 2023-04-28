7c9cce0e88d0219b1436ce60780e7b1e2fc46625
Vincenzo CDN <vincenzocdn@gmail.com>
2023-04-28 15:24:20 +0200

    Initial commit

---
+++ src/main/java/co/develhope/crud/entities/Car.java
@@ -0,0 +1,46 @@
+package co.develhope.crud.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String modelName;
+    private String type;
+
+    public Car() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
